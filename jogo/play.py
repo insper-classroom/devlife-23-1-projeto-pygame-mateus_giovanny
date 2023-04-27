b81a0d264511298c57c904791ca924b86b790aa7
MMp <mateusmoreirammp052@gmail.com>
2023-04-27 15:27:13 -0300

    terminei o menu do site, mas ainda não tá responsivo

--- jogo/play.py
+++ jogo/play.py
@@ -3,10 +3,4 @@ from classes.Jogo import Jogo
 if __name__ == '__main__':
     jogo = Jogo()
     jogo.game_loop()
-<<<<<<< HEAD
     jogo.finaliza()
-
-    
-=======
-    jogo.finaliza()
->>>>>>> b3b7ed741a279f6301b62dc64b68fc7f6ea1ca17
