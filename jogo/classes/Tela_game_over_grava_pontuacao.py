d0c234a4aea5c70019e1bfb00361a1e9458bbfba
giovannyjvr <giovannyjvrusso@gmail.com>
2023-04-29 23:14:21 -0300

    inserindo nova função para gravar resultados

--- jogo/classes/Tela_game_over_grava_pontuacao.py
+++ jogo/classes/Tela_game_over_grava_pontuacao.py
@@ -13,6 +13,12 @@ class Tela_game_over_pontuacao:
                 if 609>= evento.pos[0] >= 416 and 735 >= evento.pos[1] >=668:
                     from classes.Fase1 import Fase1
                     return Fase1(0)
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                if 841>= evento.pos[0] >= 560 and 113 >= evento.pos[1] >=78:
+                    return None
+            if evento.type == pygame.KEYDOWN:
+                print (f'apertou uma letra{letra}')
+                
         return self
 
     def text2image(self, text):
@@ -24,8 +30,8 @@ class Tela_game_over_pontuacao:
         JANELA.blit(self.imagem,(TAMANHO_JANELA[0]//2 - 700//2 ,TAMANHO_JANELA[1]//2 - 760//2))
         #pygame.draw.rect(JANELA,AZUL,pygame.Rect(372, 670, 281, 46))
         #pygame.draw.rect(JANELA,AZUL,pygame.Rect(209, 44, 281, 35))
-        JANELA.blit(self.text2image(f"{str(self.pontuacao)}"), ((194 + 281//2) - ((len(str(self.pontuacao))*15)//2), 45))
         #pygame.draw.rect(JANELA,AZUL,pygame.Rect(560, 78, 281, 35))
+        JANELA.blit(self.text2image(f"{str(self.pontuacao)}"), ((194 + 281//2) - ((len(str(self.pontuacao))*15)//2), 45))
         JANELA.blit(self.text2image(f"{str(self.pontuacao)}"), ((545 + 286//2) - ((len(str(self.pontuacao))*15)//2), 80))
 
 
